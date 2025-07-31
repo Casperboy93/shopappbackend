3c27578df0a532dcd39be0611268fa658688dcb9
mac-med <mohammed.jaize1993@gmail.com>
2025-07-31 01:53:24 +0100

    use CORS:OrigIN

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://uncomplicated.onrender.com/', // ⚠️ change this to your frontend URL in prod
-    credentials: true,
+    origin: process.env.CORS_ORIGIN,
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
