ff84dbfabd2985511478b6170823414d194bf81e
Casperboy93 <mohammed.jaize1993@gmail.com>
2025-08-03 12:46:05 +0100

    adding the port on app.cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   });
 
